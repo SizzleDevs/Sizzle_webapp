ba287a5399f88be0fdeb43835a35954eccd73a2c
Nyvo2010 <niekyuwen@gmail.com>
2026-01-07 15:27:49 +0100

    Update config.js

--- webapp/config.js
+++ webapp/config.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = 'https://sizzle-backend-9mad.onrender.com';
+const API_BASE_URL = 'https://sizzle-backend-9mad.onrender.com:5000';
 
 // API Endpoints
 const API = {
